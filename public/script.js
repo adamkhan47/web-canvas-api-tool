469915a7e6442cbf4bcfc67213dbe270c83c506c
Adam Khan <adammkhan47@gmail.com>
2025-09-27 15:00:42 -0500

    Auto get user and pfp

--- public/script.js
+++ public/script.js
@@ -1,19 +1,29 @@
 let canvasToken = "";
-window.onload = function() {
+window.onload = async () => {
     try {
         canvasToken = localStorage.getItem("token");
         if (canvasToken === null || canvasToken === undefined) {throw new Error("ok")};
+        setPfpAndUser();
     }
     catch(error) {setToken();}
 };
+
 function setToken() {
     let token = prompt("Enter your Canvas API Token");
     canvasToken = token;
     localStorage.setItem("token", token);
-    document.getElementById("token").innerHTML = canvasToken.substring(0, 4);
+    setPfpAndUser();
 }
 async function retrieveCanvasRawData() {
     const res = await fetch('/rawData?token='+canvasToken);
     const data = await res.text();
     document.getElementById("dataText").innerHTML = data;
+}
+async function setPfpAndUser() {
+    const res = await fetch('/rawData?token='+canvasToken);
+    const data = await res.text();
+    let actualData = JSON.parse(data);
+    let pfp = actualData.avatar_url;
+    let name = actualData.first_name;
+    document.getElementById("name").innerHTML = name + "  "+ '<img src="' + pfp + '" alt="Username">';
 }
