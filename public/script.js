959f2623c5c0707c6a4dad402e026cbd74c1de60
Adam Khan <adammkhan47@gmail.com>
2025-09-27 14:14:51 -0500

    currently broken but work on getting token..

--- public/script.js
+++ public/script.js
@@ -1,8 +1,14 @@
 let canvasToken;
 window.onload = function() {
+    try {
+        canvasToken = localStorage.getItem("token");
+        if (canvasToken === null || canvasToken === undefined) {throw new Error("ok")};
+    }
+    catch(error) {setToken();}
 };
-function save() {
-    let thing = document.getElementById('canvastoken').value;
+function setToken() {
+    let token = prompt("Enter your Canvas API Token");
     canvasToken = thing;
-       
+    localStorage.setItem("token", token);
+    document.getElementById("token").innerHTML = token.substring(0, 4);
 }
